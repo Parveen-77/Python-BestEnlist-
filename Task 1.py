84420e62c658ddbe8123796e1a011275094c4e13
Parveen-77 <85792553+Parveen-77@users.noreply.github.com>
2021-06-13 13:48:15 +0530

    Add files via upload

---
+++ Task 1.py
@@ -0,0 +1,23 @@
+def add(x, y):
+
+    z = x+y
+    print(z)
+
+
+add(x=int(input("enter number")), y=int(input("enter next number")))
+
+
+def maxi(n):
+    x = (max(n))
+    print("maximum is", x)
+
+
+maxi((687, 857, 749, 464, 345))
+
+
+def mini(n):
+    x = (min(n))
+    print("minimum is", x)
+
+
+mini((687, 857, 749, 464, 345))
